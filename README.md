e66f7d18ae8cbc336e746a76655f6d676b1c7942
madebyrogal <tomasz@madebyrogal.com>
2016-09-27 21:07:10 +0200

    ADD First blood

---
+++ README.md
@@ -0,0 +1,10 @@
+foodPlanner
+===========
+
+A Symfony project created on September 27, 2016, 8:33 pm.
+
+1) How to start
+
+2) Run Tests
+
+3) Know issue
---
+++ src/FoodPlanner/Application/FoodPlannerBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace FoodPlanner\Application;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * FoodPlannerBundle is a symfony application bundle
+ *
+ * @author madebyrogal
+ */
+class FoodPlannerBundle extends Bundle
+{
+    
+}
